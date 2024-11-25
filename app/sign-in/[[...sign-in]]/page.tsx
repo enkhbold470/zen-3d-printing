2b0b6d60c2ba791dc8ed9a94765d2388efd6c56c
Enkhbold470 <enkhbold470@gmail.com>
2024-11-25 02:07:32 -0800

    Refactor sign-in and sign-up pages to center content vertically

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,9 @@
-//app/sign-in/[...sign-in]/page.tsx
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
+    </div>
+  );
 }
